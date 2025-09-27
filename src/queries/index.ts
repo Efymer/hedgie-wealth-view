70a98301716c4d3f774f122ce9ad29df3de7b53a
Efymer <loscojones999@gmail.com>
2025-09-27 09:51:06 +0200

    refactor: clean up token query logging and remove unused tokenIds array

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -422,9 +422,6 @@ const useAccountTokens = (walletId: string) => {
 
 export const useAccountTokenDetails = (walletId: string) => {
   const tokensQuery = useAccountTokens(walletId);
-  const tokenIds = Array.from(
-    new Set((tokensQuery.data ?? []).map((t) => t.token_id).filter(Boolean))
-  );
 
   const infosQuery = useQuery({
     queryKey: ["token_infos", walletId],
@@ -438,7 +435,7 @@ export const useAccountTokenDetails = (walletId: string) => {
 
   const infoMap = infosQuery.data ?? {};
   console.log("infoMap", infoMap);
-  console.log("tokensQuery", tokensQuery);
+  console.log("tokensQuery data", tokensQuery.data);
 
   const details = (tokensQuery.data ?? []).map((t) => {
     const info = infoMap[t.token_id];
