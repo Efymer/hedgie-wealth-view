64dbf7dfd461a81a63ba8e8096f6132621c91c23
Efymer <loscojones999@gmail.com>
2025-09-30 13:37:35 +0200

    adjust: increase initial zoom level from 2x to 5x for better map visibility

--- src/components/CounterpartyMap.tsx
+++ src/components/CounterpartyMap.tsx
@@ -121,7 +121,7 @@ export const CounterpartyMap: React.FC<CounterpartyMapProps> = ({
       fg.d3Force("center").strength(0.1);
       
       // Zoom in slightly for better initial view
-      fg.zoom(2);
+      fg.zoom(5);
     }
   }, [graphData]);
 
